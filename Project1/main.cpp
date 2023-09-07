f478121a1a1db5b1e5b45d56543a7bdea3e749a8
hooo1273 <111989007+hooo1273@users.noreply.github.com>
2023-09-07 15:47:56 +0900

    ㄹㅇㄴㄹㅇㄴㄹㅇ

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
-int main(void)
+int main(void) 
 {
-	printf("hello wolrd\n");
-	printf("hello wolrd\n");
-	return 0;
+    int n;
+
+    scanf_s("%d", &n);
+    printf("%d층 피라미드\n", n);
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
 }
