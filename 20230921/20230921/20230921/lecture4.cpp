dfe0b706b68879b3f7e2e8cda872168294f43a34
qpalzm4049 <144310815+qpalzm4049@users.noreply.github.com>
2023-09-21 22:03:38 +0900

    몬테 카를로 수정

--- 20230921/20230921/20230921/lecture4.cpp
+++ 20230921/20230921/20230921/lecture4.cpp
@@ -2,25 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-
-int main(void) {
 	
+int main(void)
+{
 	int count = 0, circlr = 0, per = 1;
 	double x = 0;
 	double y = 0;
-	
+
 	srand(time(NULL));
 
-	while (count < 100000000) {
+	while (count < 1000000000) {
 		x = (double)rand() / (double)RAND_MAX;
 		y = (double)rand() / (double)RAND_MAX;
 		count++;
-		if (sqrt((x * x) + (y * y)) < 1) 
+		if (sqrt((x * x) + (y * y)) < 1)
 			circlr++;
 		if (count % 10000000 == 0) {
-			printf("%d%%진행.. 원주율 : %lf\n", per, (double) 4 * circlr / count  );
+			printf("%d%%진행.. 원주율 : %lf", per, (double)4 * circlr / count);
+				for (int j = 0; j < per / 5; j++ )
+					printf("■");
+				for (int k = 0; k < 20 - (per / 5); k++)
+					printf("□");
+			printf("\n");
 			per++;
 		}
 	}
-	return 0;
 }
